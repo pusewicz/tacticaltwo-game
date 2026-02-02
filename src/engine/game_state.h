e8ba3b770fa9a1403d1587ef086c18ffc23378ff
Piotr Usewicz <piotr@layer22.com>
2026-02-02 18:21:30 +0100

    Use a canvas

--- src/engine/game_state.h
+++ src/engine/game_state.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cute_alloc.h>
+#include <cute_graphics.h>
 
 #include "world.h"
 
@@ -9,6 +10,8 @@ typedef struct Platform Platform;
 typedef struct GameState {
   Platform* platform;
   CF_Arena* scratch_arena;
+
+  CF_Canvas canvas; // The main game canvas
 
   bool debug_mode;
 
